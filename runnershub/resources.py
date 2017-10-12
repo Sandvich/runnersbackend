0c34ff8d69f780b7d3f91a602b82c1a487889dc3
Sanchit Sharma <sanchitsharma1@gmail.com>
2017-10-12 22:51:20 +0100

    Character GET/PUT/DELETE and characters GET/POST now working as expected.

--- runnershub/resources.py
+++ runnershub/resources.py
@@ -1,3 +1,4 @@
+from flask import url_for, abort
 from flask_restful import Resource, reqparse
 from .models import Character, db
 
@@ -5,18 +6,49 @@ from .models import Character, db
 class CharacterAPI(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument("name", type=str, required=True, location='json')
+        self.reqparse.add_argument("name", type=str, location='json')
         self.reqparse.add_argument("description", type=str, location='json')
+        self.reqparse.add_argument("pc", type=bool, location='json')
         super(CharacterAPI, self).__init__()
 
     def get(self, id):
-        pass
+        char = Character.query.filter_by(id=id).one_or_none()
+        if (char is None):
+            abort(404)
+
+        return {"name": char.name, "description": char.description, "PC": char.pc, "URI": url_for("character", id=id)}
 
     def put(self, id):
-        pass
+        char = Character.query.filter_by(id=id).one_or_none()
+        if char is None:
+            abort(404)
+
+        args = self.reqparse.parse_args()
+
+        changed = []
+        if (args["name"] is not None) and (args["name"] != char.name):
+            char.name = args["name"]
+            changed.append("name")
+        if (args["description"] is not None) and (args["description"] != char.description):
+            char.description = args["description"]
+            changed.append("description")
+        if (args["pc"] is not None) and (args["pc"] != char.pc):
+            char.pc = args["pc"]
+            changed.append("pc")
+
+        if len(changed) > 0:
+            db.session.add(char)
+            db.session.commit()
+
+        return {"changed": changed, "URI": url_for("character", id=id)}
 
     def delete(self, id):
-        pass
+        char = Character.query.filter_by(id=id).one_or_none()
+        if char is None:
+            abort(404)
+        db.session.delete(char)
+        db.session.commit()
+        return {"status": "Success"}
 
 
 class CharacterListAPI(Resource):
@@ -30,11 +62,11 @@ class CharacterListAPI(Resource):
 
     def get(self):
         allchars = Character.query.all()
-        return [{"name": char.name, "description": char.description, "PC": char.pc, "ID": char.id} for char in allchars]
+        return [{"name": char.name, "URI": url_for("character", id=char.id)} for char in allchars]
 
     def post(self):
         args = self.reqparse.parse_args()
         char = Character(args['name'], args['description'], args['pc'])
         db.session.add(char)
         db.session.commit()
-        return {"name": char.name, "description": char.description, "PC": char.pc, "ID": char.id}
+        return {"URI": url_for("character", id=char.id)}
